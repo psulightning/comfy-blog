7c841fea2da42682fdcc099880b1e7e02026bd4c
Zach Hamman <z.hamman@gmail.com>
2014-03-28 22:52:30 -0400

    italicize author and style if not logged in

--- app/helpers/blog/posts_helper.rb
+++ app/helpers/blog/posts_helper.rb
@@ -3,6 +3,6 @@
 
 module Blog::PostsHelper
   def author_link(obj)
-    current_user.logged? ? link_to(obj.author, user_path(obj.author)) : obj.author
+    current_user.logged? ? link_to(obj.author, user_path(obj.author)) : "<i style='color:#6f686a'>#{obj.author}</i>".html_safe
   end 
 end
